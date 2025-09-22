ef28613829c6681f9b5af8411076ba7f8931fc30
Zakayo Oshome <zakayooshome254@gmail.com>
2025-09-22 20:03:13 +0000

    WHY IS THE IMAGE OF THE CONTENTFLOW AI section in the my projects not sh

--- next.config.ts
+++ next.config.ts
@@ -9,20 +9,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
